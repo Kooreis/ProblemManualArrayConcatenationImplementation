1f23afb41318eb4ca3d5271f5088a78c8d8f373d
Kooreis <kooreis.official@gmail.com>
2024-12-07 16:31:19 -0600

    Initial function setup with an empty result array

--- solution.cs
+++ solution.cs
@@ -1,14 +1,3 @@
-```javascript
 function concatenateArrays(arr1, arr2) {
     let result = [];
-    for(let i = 0; i < arr1.length; i++) {
-        result.push(arr1[i]);
-    }
-    for(let i = 0; i < arr2.length; i++) {
-        result.push(arr2[i]);
-    }
-    return result;
 }
-
-console.log(concatenateArrays([1, 2, 3], [4, 5, 6]));
-```
